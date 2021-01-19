677ae3a37aa4f07972c3735d948cc1f3cb4ea52f
Artist06 <42911670+Artist06@users.noreply.github.com>
2021-01-19 16:09:19 +0530

    Update database_registration.php

--- Hackathon 2020/database_registration.php
+++ Hackathon 2020/database_registration.php
@@ -1,94 +1,79 @@
 <?php
+session_start();
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, HEAD, OPTIONS");
+header("Access-Control-Allow-Headers: *");
 
-// Server name must be localhost 
+// Server name is localhost 
 $servername = "localhost"; 
   
 // In my case, user name will be root 
 $username = "root"; 
   
 // Password is empty 
-$password = "root"; 
+$password = ""; 
 
 //current date
-$date = date("d/m/Y");
+$date = date("Y-m-d");
 
-// cookie value
-$name='';
+$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 
-//cookie value
-$org_number='';
+if ($contentType === "application/json") {
+  //Receive the RAW post data.
+  $content = trim(file_get_contents("php://input"));
 
-//cookie value
-$ph_number='';
+  $decoded = json_decode($content, true);
 
-//cookie value
-$email='';
+  //If json_decode failed, the JSON is invalid.
+  if(! is_array($decoded)) {
+      echo "Invalid json";
 
-//image value
-$image='';
+  } else {
+      $name=$decoded['name'];
+      $org_number=$decoded['id_no'];
+      $ph_number=$decoded['contact'];
+      $email=$decoded['email'];
+      $image=$decoded['image'];
+      //$image = base64_decode(preg_replace('#^data:image/\w+;base64,#i', '', $image));
+    // Send error back to user.
+  }
+}
 
-if(isset($_COOKIE)) {
-    var_dump($_COOKIE);
-    foreach($_COOKIE as  $key => $val)
-    {
-      if($key=='name'){
-
-          $name=$val;
-      }
-      else if($key=='ID No'){
-
-          $org_number=$val;
-      }
-      else if($key=='Mobile No'){
-
-          $ph_number=$val;
-      }
-      else if($key=='Email'){
-
-          $email=$val;
-      }
-      else if($key=='Image'){
-
-          $image=$val;
-      }
-    }
-} 
- 
 // Creating a connection 
 $conn = new mysqli($servername,  
-            $username, $password); 
+            $username, $password, "Employee_information"); 
   
 // Check connection 
 if ($conn->connect_error) { 
     die("Connection failure: " 
         . $conn->connect_error); 
 }  
-  
-// Creating a database named Employee_information and table Employees 
-$sql = "CREATE DATABASE IF NOT EXISTS Employee_information"; 
-if ($conn->query($sql) === TRUE) { 
-    echo "Database with name Employee_information"; 
-} else { 
-    echo "Error: " . $conn->error; 
+if ($conn->ping()) {
+    printf ("Our connection is ok!\n");
+} else {
+    printf ("Error: %s\n", $conn->error);
 }
-$sql="CREATE TABLE IF NOT EXISTS Employees(Sl_no INT IDENTITY(1,1) , Full_name varchar(30), ID_no INT, Contact INT, Email varchar(30), registration_date DATE, ID_preview blob(10M))";
-$conn -> query($sql);
+// Creating a table Employees 
+/*$sql="CREATE TABLE IF NOT EXISTS `Employees`(Sl_no SMALLINT NOT NULL primary key AUTO_INCREMENT, Full_name varchar(30), 
+      ID_no SMALLINT, Contact varchar(10), Email varchar(30), registration_date DATE, 
+      ID_preview varchar(100))";
+$conn -> query($sql);*/
 
 // Inserting records
-mysqli_select_db($conn, "Employee_information");
-if($conn -> query("SELECT Full_name from Employees WHERE Full_name = $name") != $name){
-    $sql = "INSERT INTO Employees (Full_name, ID, Contact, Email, registration_date, ID_preview) VALUES ($name, $org_number, $ph_number, $email, $date, $image)";
-    if ($conn->query($sql) === TRUE){
-        echo "Inserted " .$conn->query("SELECT Sl_No from Employees where Full_name=$name"). "record(s)";
-    }else { 
-        echo "Error: " . $conn->error; 
-    }
-}
-else{
-    echo "Don't worry ".$name.", you are already registered.";
-}
+$stmt = $conn->prepare("INSERT INTO `Employees` (Full_name, ID_no, Contact, Email, 
+                                    registration_date, ID_preview) 
+                                    VALUES (?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("siisss", $name, $org_number, $ph_number, $email, $date, $image);
+$stmt->execute();
 
+if($stmt->execute())
+    echo "record inserted";
+else
+    echo $stmt->error;
+
+$content="";
   
 // Closing connection 
+$stmt->close();
 $conn->close(); 
 ?>
