92bd5ccce2a44f124daf9c94df6332c322676285
Abhishek Roychoudhury <42911670+Artist06@users.noreply.github.com>
2021-01-26 19:23:17 +0530

    Update database_registration.php

--- database_registration.php
+++ database_registration.php
@@ -43,7 +43,7 @@ if ($contentType === "application/json") {
 
 // Creating a connection 
 //$conn = new mysqli($servername,  $username, $password, "dd81qjvfuts0di"); 
-$conn = pg_connect("host=".($servername)." port=5432 dbname=".($database)." user=".($username)." password=".($password)." connect_timeout=5");
+$conn = pg_connect("host=".($servername)." port=5432 dbname=".($database)." user=".($username)." password=".($password)." sslmode=require");
   
 // Check connection 
 /*if ($conn->connect_error) { 
