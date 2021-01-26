170ec818459af1c370d994f0b0b8bcd8d78a4eba
Abhishek Roychoudhury <42911670+Artist06@users.noreply.github.com>
2021-01-26 19:24:12 +0530

    Update database_fetch.php

--- database_fetch.php
+++ database_fetch.php
@@ -19,7 +19,7 @@ foreach($_POST as $p){
     $name=$p;
 }
 
-$conn = pg_connect("host=".($servername)." port=5432 dbname=".($database)." user=".($username)." password=".($password)." connect_timeout=5");
+$conn = pg_connect("host=".($servername)." port=5432 dbname=".($database)." user=".($username)." password=".($password)." sslmode=require");
 
 
 /*$sql = $conn->prepare("SELECT Sl_no,Full_name from employees where Full_name = ?");
